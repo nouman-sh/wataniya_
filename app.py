2d4ff671d35ce8ba58f59072906a42cb3dadb242
nouman1815 <125795932+nouman1815@users.noreply.github.com>
2025-06-19 15:51:52 +0530

    Initial commit with all project files

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key = "your_secret_key_here"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'noumanshariff7411@gmail.com'
-app.config['MAIL_PASSWORD'] = 'qgob gpxj jgqy zhoq'  # App password
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'noumanshariff7411@gmail.com'
 
 mail = Mail(app)
